42825c7ed81314cd1f41e0b79e96622280b8cac7
Fabian Henneke <fabian.henneke@gmail.com>
2017-01-18 22:59:07 +0100

    Refactor

--- background.js
+++ background.js
@@ -3,6 +3,14 @@
   const LINK_MENU_ID = "ics2gcal.contextmenu.link";
   const LINK_MENU_SEPARATOR_ID = "ics2gcal.contextmenu.separator";
   const LINK_MENU_CALENDAR_ID_PREFIX = "ics2gcal.contextmenu.link.calendar/";
+  const LINK_MENU_CONTEXT = {
+    "contexts": ["link"],
+    "targetUrlPatterns": [
+      "*://*/*.ics",
+      "*://*/*.ICS",
+      "*://*/*ics_view*"
+    ]
+  };
 
   function handleStatus(response) {
     if (response.status >= 200 && response.status < 300) {
@@ -12,14 +20,10 @@
     }
   }
 
-  function toJson(response) {
-    return response.json();
-  }
-
   function linkMenuCalendar_onClick(info) {
-    const ics_link = info.linkUrl;
-    const calendar_id = info.menuItemId.split("/")[1];
-    fetch(ics_link)
+    const icsLink = info.linkUrl;
+    const calendarId = info.menuItemId.split("/")[1];
+    fetch(icsLink)
       .then(handleStatus)
       .then(response => response.text())
       .catch(function (error) {
@@ -32,7 +36,7 @@
       .then(function (jcalData) {
         const vevents = new ICAL.Component(jcalData).getAllSubcomponents();
         for (let vevent of vevents) {
-          createEvent(new ICAL.Event(vevent), calendar_id);
+          createEvent(new ICAL.Event(vevent), calendarId);
         }
       });
   }
@@ -41,63 +45,39 @@
     // TODO
   }
 
-  function installContextMenu(calendars, hidden_calendars) {
-    chrome.contextMenus.create({
+  function installContextMenu(calendars, hiddenCalendars) {
+    chrome.contextMenus.create(Object.assign({
       "id": LINK_MENU_ID,
-      "title": "Add to calendar",
-      "contexts": ["link"],
-      "targetUrlPatterns": [
-        "*://*/*.ics",
-        "*://*/*.ICS",
-        "*://*/*ics_view*"
-      ]
-    });
-    for (let calendar_id in calendars) {
-      chrome.contextMenus.create({
-        "id": LINK_MENU_CALENDAR_ID_PREFIX + calendar_id,
-        "title": calendars[calendar_id],
-        "parentId": LINK_MENU_ID,
-        "contexts": ["link"],
-        "targetUrlPatterns": [
-          "*://*/*.ics",
-          "*://*/*.ICS",
-          "*://*/*ics_view*"
-        ]
-      });
+      "title": "Add to calendar"
+    }, LINK_MENU_CONTEXT));
+    for (let calendarId in calendars) {
+      chrome.contextMenus.create(Object.assign({
+        "id": LINK_MENU_CALENDAR_ID_PREFIX + calendarId,
+        "title": calendars[calendarId],
+        "parentId": LINK_MENU_ID
+      }, LINK_MENU_CONTEXT));
     }
-    chrome.contextMenus.create({
+    chrome.contextMenus.create(Object.assign({
       "id": LINK_MENU_SEPARATOR_ID,
       "type": "separator",
-      "parentId": LINK_MENU_ID,
-      "contexts": ["link"],
-      "targetUrlPatterns": [
-        "*://*/*.ics",
-        "*://*/*.ICS",
-        "*://*/*ics_view*"
-      ]
-    });
-    for (let calendar_id in hidden_calendars) {
-      chrome.contextMenus.create({
-        "id": LINK_MENU_CALENDAR_ID_PREFIX + calendar_id,
-        "title": hidden_calendars[calendar_id],
+      "parentId": LINK_MENU_ID
+    }, LINK_MENU_CONTEXT));
+    for (let calendarId in hiddenCalendars) {
+      chrome.contextMenus.create(Object.assign({
+        "id": LINK_MENU_CALENDAR_ID_PREFIX + calendarId,
+        "title": hiddenCalendars[calendarId],
         "parentId": LINK_MENU_ID,
-        "contexts": ["link"],
-        "targetUrlPatterns": [
-          "*://*/*.ics",
-          "*://*/*.ICS",
-          "*://*/*ics_view*"
-        ]
-      });
+      }, LINK_MENU_CONTEXT));
     }
   }
 
-  function createEvent(event, calendar_id) {
+  function createEvent(event, calendarId) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      let tab_url = tabs[0].url;
-      let gcal_event = {
+      let tabUrl = tabs[0].url;
+      let gcalEvent = {
         'summary': event.summary,
         'location': event.location,
-        'description': `Source: ${tab_url}`,
+        'description': `Source: ${tabUrl}`,
         'start': {
           'dateTime': event.startDate.toString(),
           'timeZone': event.startDate.zone.toString()
@@ -110,22 +90,22 @@
           'useDefault': true
         }
       };
-      console.log(JSON.stringify(gcal_event));
+      console.log(JSON.stringify(gcalEvent));
       chrome.identity.getAuthToken({"interactive": false}, function(token) {
         if (chrome.runtime.lastError) {
           alert(chrome.runtime.lastError.message);
           return;
         }
 
-        fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendar_id}/events`, {
+        fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {
           method: "POST",
           headers: new Headers({
             "Content-Type": "application/json",
             "Authorization": `Bearer ${token}`
           }),
-          body: JSON.stringify(gcal_event)
+          body: JSON.stringify(gcalEvent)
         });
-    });
+      });
     });
   }
 
@@ -140,19 +120,20 @@
         method: "GET"
       })
         .then(handleStatus)
-        .then(toJson)
+        .then(response => response.json())
         .then(function (response) {
           let calendars = {};
-          let hidden_calendars = {};
+          let hiddenCalendars = {};
           for (let item of response.items) {
+            // Only consider calendars in which we can create events
             if (item.accessRole != "owner" && item.accessRole != "writer")
               continue;
             if (item.selected)
               calendars[item.id] = item.summary;
             else
-              hidden_calendars[item.id] = item.summary;
+              hiddenCalendars[item.id] = item.summary;
           }
-          installContextMenu(calendars, hidden_calendars);
+          installContextMenu(calendars, hiddenCalendars);
         })
         .catch(function (error) {
           alert("Request 'calendarList' failed: " + error);
