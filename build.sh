e8a2996f3426dd028edbbdfa095e2f80dbc03eef
Fabian Henneke <fabian.henneke@gmail.com>
2017-02-06 09:15:35 +0100

    Add simple build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+bower install
+
+mkdir -p build
+cp background.js manifest.json snackbar.css snackbar.js build
+mkdir -p build/bower_components/ical.js/build
+cp bower_components/ical.js/build/ical.min.js build/bower_components/ical.js/build
+mkdir -p build/bower_components/chrome-promise
+cp bower_components/chrome-promise/chrome-promise.js build/bower_components/chrome-promise
+cd build
+zip -r ../ics2gcal.zip *
