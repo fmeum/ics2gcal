341b2f315c13dd062e5424b5eab1b169f36e3b60
Fabian Henneke <fabian.henneke@gmail.com>
2017-02-07 11:57:34 +0100

    Include images in build step

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ mkdir -p build/bower_components/ical.js/build
 cp bower_components/ical.js/build/ical.min.js build/bower_components/ical.js/build
 mkdir -p build/bower_components/chrome-promise
 cp bower_components/chrome-promise/chrome-promise.js build/bower_components/chrome-promise
+mkdir -p build/images
+cp images/*.png build/images/
 cd build
 zip -r ../ics2gcal.zip *
